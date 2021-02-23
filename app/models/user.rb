83555c55d50a6354786e9ed9ea87e6d111f1c335
Tak-Ohta <sacredfield.tak@gmail.com>
2021-02-23 14:32:18 +0900

    Finish full-fledged-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save {self.email = email.downcase}
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -11,4 +12,45 @@ class User < ApplicationRecord
   validates :designated_work_end_time, presence: true
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+
+  def User.digest(string)
+    cost =
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
+  def self.import(file)
+    CSV.foreach(file.path, headers: true, skip_blanks: true) do |row|
+      user = find_by(id: row["id"]) || new
+      user.attributes = row.to_hash.slice(*updatable_attributes)
+      user.save!
+    end
+  end
+
+  def self.updatable_attributes
+    ['id', 'name', 'email', 'affiliation', 'employee_number', 'uid', 'password', 'basic_work_time',
+      'designated_work_start_time', 'designated_work_end_time', 'superior', 'admin']
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210223042543) do
+ActiveRecord::Schema.define(version: 20210223052807) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20210223042543) do
     t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
