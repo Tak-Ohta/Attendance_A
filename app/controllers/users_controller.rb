ff81b03d74fa40f8f633b74f72c0eb14d9f312d5
Tak-Ohta <sacredfield.tak@gmail.com>
2021-02-23 14:17:01 +0900

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,68 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :edit_overtime_application]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:index, :destroy]
+  before_action :set_one_month, only: :show
+
   def new
+    @user = User.new
   end
+
+  def show
+    # 出勤日数
+    @worked_sum = @attendances.where.not(started_at: nil).count
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "ユーザー情報を新規登録しました。"
+      redirect_to @user
+    else
+      flash[:danger] = "ユーザー登録に失敗しました。やり直してください。"
+      render :new
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 10)
+  end
+
+  def import
+    User.import(params[:file])
+    redirect_to users_url
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      flash[:danger] = "ユーザー情報を更新できませんでした。"
+      render :edit      
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+
+  def at_work
+    @users = User.all.includes(:attendances)
+    
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :affiliation, :employee_number, :uid,
+                                  :password, :password_confirmation, :basic_work_time,
+                                  :designated_work_start_time, :designated_work_end_time)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,29 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  root 'static_pages#top'
+  root 'sessions#new'
+  get '/signup', to: 'users#new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :users do
+    member do
+      get 'attendances/edit_one_month'
+      patch 'attendances/update_one_month'
+      get 'attendances/edit_overtime_application'
+      patch 'attendances/update_overtime_application'
+    end
+
+    resources :attendances, only: :update
+    collection { post :import}
+    collection do
+      get :at_work
+    end
+  end
+
+  resources :base_points, except: :show
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
